254ba3e2f22c018a0ac556b5183ca4d2adf2e329
S-a-b-a-r-i-M <sabarinithi2002@gmail.com>
2023-11-06 19:30:14 +0530

    updation

--- sampleProject/nameGenerateApp/views.py
+++ sampleProject/nameGenerateApp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 def display(request):
+
+    time=str(datetime.date.today)
+
     return render(request,'start.html',{'names':'sabari'})
